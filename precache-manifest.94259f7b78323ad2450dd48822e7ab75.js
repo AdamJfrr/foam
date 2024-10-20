7a85eefb840abe64857aa0182e49e16b7615c2a8
adhamj <adham-jaafar@outlook.com>
2024-10-20 20:25:13 +0200

    Updates

---
+++ precache-manifest.94259f7b78323ad2450dd48822e7ab75.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a70096f4772d7986f049b39f9fb3f0b",
+    "url": "/foam/index.html"
+  },
+  {
+    "revision": "1ea139ef6aa676cd00a5",
+    "url": "/foam/static/css/main.1c059439.chunk.css"
+  },
+  {
+    "revision": "924fe6e75cf453fe1578",
+    "url": "/foam/static/js/2.eaf3e59b.chunk.js"
+  },
+  {
+    "revision": "bf91464133fd4456c9254d07a0a2ec60",
+    "url": "/foam/static/js/2.eaf3e59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca427df57760f429851",
+    "url": "/foam/static/js/3.cd112139.chunk.js"
+  },
+  {
+    "revision": "1ea139ef6aa676cd00a5",
+    "url": "/foam/static/js/main.ff9e3659.chunk.js"
+  },
+  {
+    "revision": "a33afe0803218033ee2f",
+    "url": "/foam/static/js/runtime-main.89ee7cc0.js"
+  },
+  {
+    "revision": "25071db2189f6aafdc10f74052abf449",
+    "url": "/foam/static/media/about.25071db2.jpg"
+  },
+  {
+    "revision": "17f3b2ef3a54a2a14a499de77a7f4760",
+    "url": "/foam/static/media/bubble.17f3b2ef.png"
+  },
+  {
+    "revision": "6407e8b2d2359f9dbead7b699c31a7cd",
+    "url": "/foam/static/media/green.6407e8b2.png"
+  },
+  {
+    "revision": "238d3913c3fbb0c79634e36b90a03c9b",
+    "url": "/foam/static/media/orange.238d3913.png"
+  },
+  {
+    "revision": "2c37687dc9379dcbbef63a9299b5f9db",
+    "url": "/foam/static/media/peach.2c37687d.png"
+  },
+  {
+    "revision": "08513c94249662c48c088aa68b6f72c5",
+    "url": "/foam/static/media/purple.08513c94.png"
+  }
+]);
